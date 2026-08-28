f82b1f6c72765dd2f93b5916e51d5fcb6dd63478
SUBHAM533528 <subhammaharana412@gmail.com>
2026-08-28 16:09:24 +0530

    change the footer

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -988,7 +988,7 @@ export default function Landing() {
           <div className="flex items-center gap-3">
             <img
               src="/logo2.png"
-              className="h-16 w-16 object-contain"
+              className="h-20 w-20 object-contain"
               alt="BidArenaX"
             />
           </div>
